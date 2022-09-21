f267103f4d94ba874443cbc0c6dc2a2f354a5894
Alan Chodyniecki <alan.chodyniecki@softwire.com>
2022-09-21 17:32:09 +0100

    added create validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 1 }
+  validates :author, presence: true, length: { minimum: 1 }
+  validates :ISBN, presence: true
+  validates_format_of :ISBN, with: /[0-9]{10}|[0-9]{13}]/
 end
