490a0ebe2fe75e5ff5c1f309642b7de82df5f1a2
Alan Chodyniecki <alan.chodyniecki@softwire.com>
2022-09-22 14:52:19 +0100

    Added Copy Table

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
+  has_many :copies
   validates :title, presence: true, length: { minimum: 1 }
   validates :author, presence: true, length: { minimum: 1 }
   validates :ISBN, presence: true
